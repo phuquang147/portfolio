eebd92664016a8da9b4005edb62b895edb29a711
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-07-19 21:41:41 +0700

    Add portfolio page

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import Project from '~/components/Project';
+import Heading from '~/ui-components/Heading';
 
 export default function Portfolio() {
-  return <div className="pt-[60px]">Portfolio</div>;
+  return (
+    <div className="">
+      <Heading content="Portfolio" />
+      <div className="grid grid-cols-2 container mx-auto gap-10 py-20 px-6 md:px-10 lg:px-0">
+        <Project className="col-span-2 lg:col-span-1" />
+        <Project className="col-span-2 lg:col-span-1" />
+      </div>
+    </div>
+  );
 }
