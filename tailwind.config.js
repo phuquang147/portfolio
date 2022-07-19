b0bd585c46f67a2a212faaa45fe13918f662769b
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-07-19 21:39:04 +0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,24 @@ module.exports = {
         'red-primary': '#ed3833',
         'text-gray': '#999999',
         'light-pink': '#fac4c2',
+        'light-orange': '#f07867',
       },
       backgroundImage: {
         'main-bg': "url('/src/assets/images/bg.svg')",
+        'section-1-bg': "url('/src/assets/images/bg_section1.svg')",
+        'section-contact-bg': "url('/src/assets/images/bg_section_contact.svg')",
+        'heading-bg': "url('/src/assets/images/bg_heading.svg')",
+        'project-bg': "url('/src/assets/images/project/bg_project.svg')",
+        'project-bg2': "url('/src/assets/images/project/bg_project2.svg')",
       },
       height: {
         'header-height': '70px',
+      },
+      spacing: {
+        'header-height': '70px',
+      },
+      fontFamily: {
+        'public-sans': ['"Public Sans"', 'sans-serif'],
       },
     },
   },
