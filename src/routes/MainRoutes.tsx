5ef43f4e409e4528cf4220bf7b1279f599cf942e
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-07-19 21:42:16 +0700

    Update route

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -2,6 +2,8 @@ import Home from '~/pages/Home';
 import Portfolio from '~/pages/Portfolio';
 import Contact from '~/pages/Contact';
 import MainLayout from '~/layout/MainLayout';
+import ProjectDetail from '~/pages/ProjectDetail';
+import About from '~/pages/About';
 
 const MainRoutes = {
   path: '/',
@@ -12,8 +14,16 @@ const MainRoutes = {
       element: <Home />,
     },
     {
+      path: '/about',
+      element: <About />,
+    },
+    {
       path: '/portfolio',
       element: <Portfolio />,
+    },
+    {
+      path: '/portfolio/:projectId',
+      element: <ProjectDetail />,
     },
     {
       path: '/contact',
